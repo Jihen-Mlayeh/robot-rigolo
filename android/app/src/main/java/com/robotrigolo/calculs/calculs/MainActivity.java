7ce0702ef852b14957dd2c657e3eb778f63c1318
Xenender <valentin64800@outlook.fr>
2025-02-18 17:48:06 +0100

    first commit

---
+++ android/app/src/main/java/com/robotrigolo/calculs/calculs/MainActivity.java
@@ -0,0 +1,6 @@
+package com.robotrigolo.calculs.calculs;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/ButtonMain.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/services.dart';
+
+class ButtonMain extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  final IconData? icon;
+
+  const ButtonMain({
+    Key? key,
+    required this.text,
+    required this.onPressed,
+    this.icon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: (_) => HapticFeedback.lightImpact(), // Effet de vibration léger
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 200),
+        curve: Curves.easeInOut,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.orangeAccent, Colors.deepOrange],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.orange.withOpacity(0.5),
+              blurRadius: 10,
+              offset: Offset(2, 4),
+            ),
+          ],
+        ),
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: onPressed,
+            borderRadius: BorderRadius.circular(20),
+            splashColor: Colors.white.withOpacity(0.3),
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 14, horizontal: 24),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  if (icon != null) ...[
+                    Icon(icon, color: Colors.white, size: 24),
+                    SizedBox(width: 8),
+                  ],
+                  Text(
+                    text,
+                    style: GoogleFonts.comicNeue(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
